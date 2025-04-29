99edac11ee2cb05f266ef91374c622871b7d3241
“Amber <2909415o@student.gla.ac.uk>
2025-04-29 21:42:02 +0100

    tests fixes on gitignore

--- bot-env/bot_token.py
+++ bot-env/bot_token.py
@@ -3,8 +3,4 @@ def init():
     # It should not be shared with anyone
     # Replace 'add your bot token here' with your actual bot token
     global BOT_TOKEN
-<<<<<<< HEAD:bot-env/BOT_TOKEN.py
     BOT_TOKEN = 'add your bot token here'  # Replace with your actual bot token
-=======
-    BOT_TOKEN = 'add your bot token here'  # Replace with your actual bot token
->>>>>>> 9000f7c35fab42a676f0817f3e316be999a5f604:bot-env/bot_token.py
